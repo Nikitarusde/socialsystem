d807002738996c49eaf8c9d0e9f52da40e3d8118
Nikita <nikistosov@gmail.com>
2021-12-08 21:57:24 +0100

    step10

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+
+export const Header = () => {
+    return (
+        <div>
+            <a href='#S'>Home</a> -
+            <a href='#S'>News Feed</a> -
+            <a href='#S'>JS</a> -
+        </div>
+    );
+}
