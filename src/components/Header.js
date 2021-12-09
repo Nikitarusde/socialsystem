131c7d6a382d6b6f1540c8d521697e2fa01bf926
Nikita <nikistosov@gmail.com>
2021-12-09 19:45:17 +0100

    step14

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import classes from './Header.module.css';
+
+export const Header = () => {
+   return (
+    <header className={classes.header}>
+        <img src='https://cdn.logo.com/hotlink-ok/logo-social.png'/>
+    </header>
+   );
+}
