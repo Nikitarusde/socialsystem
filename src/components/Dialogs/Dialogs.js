1afaecc6bb286b401eb84450668831e56cb919d2
Nikita <nikistosov@gmail.com>
2021-12-11 19:40:44 +0100

    step18

---
+++ src/components/Dialogs/Dialogs.js
@@ -0,0 +1,9 @@
+import classes from "./Dialogs.module.css";
+
+export const Dialogs = (props) => {
+    return(
+        <div>
+            Dialogs
+        </div>
+    )
+}
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,7 +3,7 @@ import {MyPosts} from "./MyPosts/MyPosts";
 
 export const Profile = () => {
    return (
-       <div className={classes.content}>
+       <div>
            <div>
                <img src='https://www.lufthansa.com/content/dam/lh/images/pixels_variations/c-374116967-7787557.jpg.transform/lh-dcep-transform-width-1440/img.jpg'/>
            </div>
